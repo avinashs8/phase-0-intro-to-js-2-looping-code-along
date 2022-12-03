50536d4b61f9a48573c9ef1c2e43f03c1485d3f8
avinash singh <avinash8.as@gmail.com>
2022-12-03 18:34:16 -0500

    commit tree 9c783905420c4322c0689f55dcdf26944d5d19c8

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+const { default: NormalizeOpts } = require("@babel/core/lib/transformation/normalize-opts")
+
 // Code your solutions in this file
+
+function writeCards (name, event){
+    let greeting = []
+    for (let i=0; i< name.length; i++){
+        greeting.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+    }
+    return greeting
+}
+
+function countDown(i){
+    while (i >0){
+        console.log(i)
+        i --
+    } 
+    console.log(i)
+}
+
